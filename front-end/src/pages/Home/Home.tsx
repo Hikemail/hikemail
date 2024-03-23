9589baba371712430ee75086e4e49ad49801dd27
SamrutGadde <samrut.gadde@gmail.com>
2024-03-23 03:25:15 -0500

    home page restyle

--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import './Home.css'
-import { useNavigate } from 'react-router-dom'
-import StackedWaves from '../../assets/stacked-peaks-haikei.svg'
-
+import React from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
+import HikeLogo from "../../assets/hike-logo.png";
+import BlobScene from "../../assets/blob-scene-haikei.svg";
 
 export default function Home() {
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   return (
     <div className="home-container">
+      <div className="home-showoff">
+        <div className="home-brand-header">
+          <div className="home-title">hike</div>
+          <img src={HikeLogo} alt="Hike Logo" className="home-logo" />
+        </div>
+        <TypeAnimation
+          cursor={false}
+          sequence={[
+            "Make applying to jobs easier.",
+            2000,
+            "Make applying to jobs more organized.",
+            2000,
+            "Make applying to jobs less stressful.",
+            2000,
+            "Make applying to jobs more efficient.",
+            2000,
+          ]}
+          wrapper="h1"
+          speed={50}
+          repeat={Infinity}
+          className="home-showoff-text"
+        />
+      </div>
       <div className="home-background"></div>{" "}
       <div className="home-card home-header">
         <div className="home-primary-card">
-          <h1>Hike</h1>
-          <h3>Revolutionize how you apply.</h3>
+          <h3>Revolutionize the application process.</h3>
         </div>
         <div
           className="home-secondary-card home-login"
